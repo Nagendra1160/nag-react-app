00e441debf6e3a309c842b938abc82d8b2f59415
Nagendra1160 <nagendrathoram66@gmail.com>
2020-06-22 18:13:21 +0530

    Updates

---
+++ precache-manifest.e0c197cdc78b6ddfae926631d4e7ce85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d418a91bc417a31dcc146d8daf632f23",
+    "url": "/nag-react-app/index.html"
+  },
+  {
+    "revision": "1bc2e84327d8d2b6b53e",
+    "url": "/nag-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8bc036127d3f26b06909",
+    "url": "/nag-react-app/static/js/2.16960475.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nag-react-app/static/js/2.16960475.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bc2e84327d8d2b6b53e",
+    "url": "/nag-react-app/static/js/main.3adfd6de.chunk.js"
+  },
+  {
+    "revision": "4cbf36bd4ebf473dc6d9",
+    "url": "/nag-react-app/static/js/runtime-main.47d70acc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nag-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
